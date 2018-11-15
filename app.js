7611bb21478ecca887319c7964859313cef6d039
Habenzy <robertrstauss@gmail.com>
2018-11-15 15:38:48 -0500

    initial commit

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const publicDir = $path.resolve('./public');
+const libDir = $path.resolve('./lib')
+import Message from 'message'
 
 app.use(express.static('public'));
 app.listen(port, () => console.log(`Chat app listening on port ${port}!`));
+
+app.get('/chat', (request, response) => {
+  const allMessages = getAllMessages()
+  response.sendFile(allMessages)
+})
